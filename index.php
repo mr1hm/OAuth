5864dbda6e870b3f62f4337af4a09361ad71783a
Kevin <kevinihm@gmail.com>
2020-01-23 12:05:58 -0800

    attempted connecting to eventbrite api

--- index.php
+++ index.php
@@ -22,23 +22,26 @@ if (isset($_SESSION['username'])) {
   die();
 }
 
-$client_id = '0oa11olndmbJx5HW54x6';
-$client_secret = 'w2b1N-LeORE99c9LItfuNN4777BMrRhHPRD7Dswq';
-$redirect_uri = 'http://localhost:8080/';
-$metadata_url = 'https://dev-741731.okta.com/oauth2/default/.well-known/oauth-authorization-server';
+$client_id = 'OZGWWSZNDOMLKMHOGX';
+$client_secret = 'XKWR6FYLGEOAVPZIPSJWCXGZM5AR33SQ4LO35ATJNMIGLHW4CT';
+$redirect_uri = 'http://localhost:8080';
+$metadata_url = 'https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=OZGWWSZNDOMLKMHOGX&redirect_uri=http://localhost:8080';
 $metadata = http($metadata_url);
 
 if (isset($_GET['code'])) { // Error Check
+  echo '<p>code exists in get</p>';
   if ($_SESSION['state'] != $_GET['state']) die('Authorization server returned invalid state parameter'); // Mitigate CSRF
   if (isset($_GET['error'])) die('Authorization server returned an error: ' . htmlspecialchars($_GET['error']));
 
-  $response = http($metadata->token_endpoint, [
-  'grant_type' => 'authorization_code',
-  'code' => $_GET['code'],
-  'redirect_uri' => $redirect_uri,
-  'client_id' => $client_id,
-  'client_secret' => $client_secret,
-]);
+  $response = http($metadata, [
+    'grant_type' => 'authorization_code',
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'code' => $_GET['code'],
+    'redirect_uri' => $redirect_uri,
+  ]);
+
+  echo "<p>{$_GET}</p>";
 
   if (!isset($response->access_token)) die('Error fetching access token');
 
@@ -58,7 +61,7 @@ if (isset($_GET['code'])) { // Error Check
 if (!isset($_SESSION['username'])) {
   $_SESSION['state'] = hash('sha256', session_id());
   // bin2hex(random_bytes(5));
-  $authorize_url = $metadata->authorization_endpoint.'?'.http_build_query([
+  $authorize_url = $metadata->token_endpoint.'?'.http_build_query([
     'response_type' => 'code',
     'client_id' => $client_id,
     'redirect_uri' => $redirect_uri,
